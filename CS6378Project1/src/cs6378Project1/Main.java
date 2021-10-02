13ec0dc72b7382ce036742fbe84b86b8d7c729a3
Guillermo Vazquez <36741158+gena217@users.noreply.github.com>
2021-10-02 18:14:12 -0500

    Add files via upload

--- CS6378Project1/src/cs6378Project1/Main.java
+++ CS6378Project1/src/cs6378Project1/Main.java
@@ -3,28 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         int nodes = 5;
-        for(int i = 0; i < nodes; ++i){
-            new Helper(i).start();
-        }
-       
-    }
-
-    public static class Helper extends Thread {
-
-        private int id;
-
-        public Helper(int id) {
-            this.id = id;
-        }
-
-        @Override
-        public void start() {
-            System.out.println("Starting node: " + id);
-            Node myNode = new Node(new NodeID(id), "config.txt", null);
+        for (int i = 0; i < nodes; ++i) {
+            Node myNode = new Node(new NodeID(i), "config.txt", null);
             new Thread(myNode).start();
         }
 
     }
-
 }
-
