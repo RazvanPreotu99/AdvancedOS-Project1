e8c7d03303ce76f0689df2e83694689e21bfaea3
Guillermo Vazquez <36741158+gena217@users.noreply.github.com>
2021-10-01 21:41:40 -0500

    first idea for creating a node

--- CS6378Project1/src/cs6378Project1/Main.java
+++ CS6378Project1/src/cs6378Project1/Main.java
@@ -1,16 +1,8 @@
-package cs6378Project1;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-		NodeID identifier = new NodeID(64);
-		String configFile = "src/cs6378Project1/config.txt";
-		
-		Application application = new Application(identifier, configFile);
-		
-		Node node = new Node(identifier, configFile, application);
-		
-		System.out.println("Number of nodes is: " + Integer.toString(node.getNumNodes()));
-	}
+    public static void main(String[] args) {
+        Node myNode = new Node( new NodeID(0), "config.txt", null);
+        System.out.println(myNode);
+        myNode.printNeighborInfo();
+    }
 }
--- CS6378Project1/src/cs6378Project1/Node.java
+++ CS6378Project1/src/cs6378Project1/Node.java
@@ -1,89 +1,165 @@
-package cs6378Project1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.net.ServerSocket;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Node{
-	// node identifier
-	private NodeID identifier;
-	
-	private Listener listener;
-	private int numNodes;
-	private NodeID [] neighbors;
-	
-	// constructor
-	public Node(NodeID identifier, String configFile, Listener listener){
-		
-		this.identifier = identifier;
-		this.listener = listener;
-		
-		int lineCounter = 0;
-		
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(configFile));
-			String line;
-			
-			boolean readNumNodes = false;
-				
-			while((line = reader.readLine()) != null && !(readNumNodes)) {
-				lineCounter ++;
-				StringTokenizer tokenizer = new StringTokenizer(line, " ", false);
-				int firstToken = toUnsignedInteger(tokenizer.nextToken());
-				if(firstToken != -1){
-					{
-						if((tokenizer.countTokens() == 0) || (Character.compare('#', tokenizer.nextToken().charAt(0))) == 0){
-							numNodes = firstToken;
-							break;
-						}
-					}
-				}
-			}
-			
-			reader.close();
-			
-			System.out.println("Number of nodes was read on line " + Integer.toString(lineCounter));
-			
-			for(int i = 0; i < numNodes; i++) {
-				NodeID id = new NodeID(i);
-				new Thread(new NodeThread(id, configFile));
-			}
-			
-		} catch (IOException e) {
-			System.out.println(e.toString());
-		}
-	}
-	
-	// checks if input string is an unsigned integer
-	public int toUnsignedInteger(String input) {
-		try {
-			int num = Integer.parseInt(input);
-			if(num >= 0)
-				return num;
-			else
-				return -1;
-		}
-		catch (Exception e) {
-			return -1;
-		}
-	}
-	
-	public NodeID[] getNeighbors() {
-		return neighbors;
-	}
-	
-	public int getNumNodes(){
-		return numNodes;
-	}
-		
-	public void send( Message message, NodeID destination) {
-		
-	}
-	public void sendToAll( Message message) {
-		
-	}
-	public void tearDown() {
-		
-	}
+//Object to reprsents a node in the distributed system
+class Node {
+
+    // node identifier
+    private NodeID identifier;
+    private Listener listener;
+
+    private NodeStruct myInfo;
+    private NodeStruct allNodes[];
+    // constructor
+
+    public Node(NodeID identifier, String configFile, Listener listener) {
+        this.identifier = identifier;
+        this.listener = listener;
+
+        myInfo = new NodeStruct(identifier, "", -1);
+        createFromFile(configFile);
+
+        //System.out.println("Finished parsing info");
+        
+        try {
+            ServerSocket ss = new ServerSocket(myInfo.port);
+        } catch (IOException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+        // TO DO
+        // start listening 
+        
+        
+        // TO DO
+        // how to establish connection to neighbor?
+        // neighbor might not be up, so either
+        // 1) keep trying until it finds it is up
+        // 2) only try when there is a message for that neighbor, give error if neighbor is down
+        // 3) ???
+    }
+
+    @Override
+    public String toString() {
+        return myInfo.toString();
+    }
+
+    // methods
+    public NodeID[] getNeighbors() {
+        return myInfo.neighbors;
+    }
+
+    public void send(Message message, NodeID destination) {
+        //Your code goes here
+    }
+
+    public void sendToAll(Message message) {
+        //Your code goes here
+    }
+
+    public void tearDown() {
+        //Your code goes here
+    }
+
+    /*
+        methods to check the configfile
+     */
+    private void createFromFile(String configFile) {
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(new File(configFile)));
+            String line;
+            int n = -1;
+
+            // find the first line for the number of nodes
+            while (n == -1 && (line = br.readLine().trim()) != null) {
+                if (isValidLine(line)) {
+                    n = Integer.parseInt(trimComments(line));
+                }
+            }
+
+            //System.out.println("There are " + nodes + " nodes");
+            //getting ids and other info
+            allNodes = new NodeStruct[n];
+            int c = 0, ownInfoIndex = -1;
+            while (c < n && (line = br.readLine().trim()) != null) {
+                if (isValidLine(line)) {
+                    String[] info = trimComments(line).split("\\s+");
+                    allNodes[c] = new NodeStruct();
+                    allNodes[c].id = new NodeID(Integer.parseInt(info[0]));
+                    if (allNodes[c].id.getID() == identifier.getID()) {
+                        ownInfoIndex = c;
+                        myInfo.machine = info[1];
+                        myInfo.port = Integer.parseInt(info[2]);
+
+                    }
+                    allNodes[c].machine = info[1];
+                    allNodes[c].port = Integer.parseInt(info[2]);
+                    ++c;
+                }
+            }
+
+            // get neighbors 
+            c = 0;
+            while (c < n && (line = br.readLine().trim()) != null) {
+                if (isValidLine(line)) {
+                    if (c == ownInfoIndex) {
+                        String temp[] = trimComments(line).split("\\s+");
+                        myInfo.neighbors = new NodeID[temp.length];
+                        for (int i = 0; i < temp.length; ++i) {
+                            myInfo.neighbors[i] = new NodeID(Integer.parseInt(temp[i]));
+                        }
+                    }
+                    ++c;
+                }
+            }
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private boolean isValidLine(String line) {
+        int n = line.charAt(0) - '0';
+        return n >= 0 && n <= 9;
+    }
+
+    private String trimComments(String line) {
+        int i = line.indexOf('#');
+        if (i == -1) {
+            return line;
+        }
+        return line.substring(0, i);
+    }
+
+    // TO DO change to private
+    // TO DO binary search or sth  faster
+    public NodeStruct findNodeStruct(NodeID id){
+        for( NodeStruct ns: allNodes){
+            if ( ns.id.getID() == id.getID())
+                return ns;
+        }
+        return null; // we should not be here
+    }
+    
+    // TO DO delete this or make private, only public
+    public void printNeighborInfo(){
+        System.out.println("Neighbors are: ");
+        for(NodeID nid: myInfo.neighbors){
+            System.out.println(findNodeStruct(nid));
+        }
+    }
 }
